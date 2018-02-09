7591ccbe22bb49f5df99e013f759fd0192b4ad92
elber <willsmithte@gmail.com>
2018-02-09 13:55:52 +0000

    pass

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  
-  def gday
-    render html: "Hello World"
+
+  def hello
+    render html: "hello, world!"
   end
 end
